3491dc471bc10f992c13365a7b0f6718f9e88453
RedSunRnD <vvatutin.rs@gmail.com>
2024-09-23 22:36:28 +0300

    Homework

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String name;
+    private Author author;
+    private int year;
+
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
+
